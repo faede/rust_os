bc0036f3ca79b12e0f3a71f1b7a127b126050dfe
faede <2554622161@qq.com>
2020-04-26 18:53:22 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+//src/main.rs
+
 #![no_std]
 #![no_main]
+
 use core::panic::PanicInfo;
+
 #[panic_handler]
 fn panic(_info:&PanicInfo)-> ! {
 	loop{}
 }
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
 	loop{}
